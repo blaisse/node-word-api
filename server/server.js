e7b4308ecf8f0aed78981b535246450b7c8d2900
Blaise <imbasheep@gmail.com>
2017-07-19 01:29:00 +0200

    CORS back

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.post("/words", (req, res) => {
