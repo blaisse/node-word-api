55834ab0afac7430c313194067c3a8d4abe7f800
Blaise <imbasheep@gmail.com>
2017-07-19 12:05:01 +0200

    PATCH add

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
